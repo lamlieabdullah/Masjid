89c0c2e6daedbeb92cad68f17c1461d861016663
lamlieabdullah <76871118+lamlieabdullah@users.noreply.github.com>
2021-03-31 02:13:35 +0800

    Update script.php

--- script.php
+++ script.php
@@ -296,7 +296,31 @@ function get_tarikh_oee(){
         
 }
 
+function formatDate(date) {
+  var monthNames = ["Januari", "Februari", "MarMac", "April", "Mei", "Jun", "Julai", "Ogos", "September", "Oktober", "November", "December"];
+  
+  var day = date.getDate();
+  	if (day < 10) { day = "0" + day };
+  var monthIndex = date.getMonth();
+  var year = date.getFullYear();
+    
+	//pull the last two digits of the year
+	year = year.toString();
+
+  return day + '-' + monthNames[monthIndex] + '-' + year;
+}
+
 function get_tarikh(){
+	var iMonthNames = new Array("Muharram","Safar","Rabiul Awal","Rabiul Akhir",
+	"Jamadil Awal","Jamadil Akhir","Rejab","Syaban",
+	"Ramadan","Syawal","Zulkaedah","Zhulhijjah");
+
+	
+	var iDate = kuwaiticalendar(<?php echo $adjustment; ?>);
+//	var outputIslamicDate = wdNames[iDate[4]] + ", " 
+//	+ iDate[5] + " " + iMonthNames[iDate[6]] + " " + iDate[7] + " AH";
+	var outputIslamicDate = iDate[5] + " " + iMonthNames[iDate[6]] + " " + iDate[7];
+	
     var tahun = moment().year();
     var maghrib = toDate(document.getElementById("maghrib").textContent);
     console.log(maghrib);
@@ -312,12 +336,18 @@ function get_tarikh(){
     $.getJSON("waktuSolat/"+tahun+"/hijrah.json", function(result){
         var tarikh_masihi  = result[hariTahunMasihi].TarikhMiladi;
         var tarikh_hijrah  = result[hariTahunHijrah].TarikhHijri;
+        
         document.getElementById("hari-masihi").innerHTML = result[hariTahunMasihi].Hari.toUpperCase();
         
-        var pecah_tarikh_hijrah = tarikh_hijrah.split("-");
-        document.getElementById("tarikh-hijrah").innerHTML = pecah_tarikh_hijrah[0];
-        document.getElementById("bulan-hijrah").innerHTML = pecah_tarikh_hijrah[1].toUpperCase();
-        document.getElementById("tahun-hijrah").innerHTML = pecah_tarikh_hijrah[2];
+//        var pecah_tarikh_hijrah = tarikh_hijrah.split("-");
+//        document.getElementById("tarikh-hijrah").innerHTML = pecah_tarikh_hijrah[0];
+//        document.getElementById("bulan-hijrah").innerHTML = pecah_tarikh_hijrah[1].toUpperCase();
+//        document.getElementById("tahun-hijrah").innerHTML = pecah_tarikh_hijrah[2];
+
+        document.getElementById("tarikh-hijrah").innerHTML = iDate[5];
+        document.getElementById("bulan-hijrah").innerHTML = iMonthNames[iDate[6]].toUpperCase();
+        document.getElementById("tahun-hijrah").innerHTML = iDate[7];		
+		
         
         var pecah_tarikh_masihi = tarikh_masihi.split("-");   
         document.getElementById("tarikh-masihi").innerHTML = pecah_tarikh_masihi[0];
@@ -325,6 +355,106 @@ function get_tarikh(){
         document.getElementById("tahun-masihi").innerHTML = pecah_tarikh_masihi[2];
     });
         
+}
+//Convert Hijri
+//http://stackoverflow.com/questions/5177598/converting-gregorian-date-to-hijri-date
+function gmod(n,m){
+return ((n%m)+m)%m;
+}
+
+function kuwaiticalendar(adjust){
+	var today = new Date();
+	if(adjust) {
+		adjustmili = 1000*60*60*24*adjust; 
+		todaymili = today.getTime()+adjustmili;
+		today = new Date(todaymili);
+	}
+	day = today.getDate();
+	month = today.getMonth();
+	year = today.getFullYear();
+	m = month+1;
+	y = year;
+	if(m<3) {
+		y -= 1;
+		m += 12;
+	}
+	
+	a = Math.floor(y/100.);
+	b = 2-a+Math.floor(a/4.);
+	if(y<1583) b = 0;
+	if(y==1582) {
+		if(m>10)  b = -10;
+		if(m==10) {
+			b = 0;
+			if(day>4) b = -10;
+		}
+	}
+	
+	jd = Math.floor(365.25*(y+4716))+Math.floor(30.6001*(m+1))+day+b-1524;
+	
+	b = 0;
+	if(jd>2299160){
+		a = Math.floor((jd-1867216.25)/36524.25);
+		b = 1+a-Math.floor(a/4.);
+	}
+	bb = jd+b+1524;
+	cc = Math.floor((bb-122.1)/365.25);
+	dd = Math.floor(365.25*cc);
+	ee = Math.floor((bb-dd)/30.6001);
+	day =(bb-dd)-Math.floor(30.6001*ee);
+	month = ee-1;
+	if(ee>13) {
+		cc += 1;
+		month = ee-13;
+	}
+	year = cc-4716;
+	
+	
+	wd = gmod(jd+1,7)+1;
+	
+	iyear = 10631./30.;
+	epochastro = 1948084;
+	epochcivil = 1948085;
+	
+	shift1 = 8.01/60.;
+	
+	z = jd-epochastro;
+	cyc = Math.floor(z/10631.);
+	z = z-10631*cyc;
+	j = Math.floor((z-shift1)/iyear);
+	iy = 30*cyc+j;
+	z = z-Math.floor(j*iyear+shift1);
+	im = Math.floor((z+28.5001)/29.5);
+	if(im==13) im = 12;
+	id = z-Math.floor(29.5001*im-29);
+	
+	var myRes = new Array(8);
+	
+	myRes[0] = day; //calculated day (CE)
+	myRes[1] = month-1; //calculated month (CE)
+	myRes[2] = year; //calculated year (CE)
+	myRes[3] = jd-1; //julian day number
+	myRes[4] = wd-1; //weekday number
+	myRes[5] = id; //islamic date
+	myRes[6] = im-1; //islamic month
+	myRes[7] = iy; //islamic year
+	
+	return myRes;
+}
+
+function writeIslamicDate(adjustment) {
+	var wdNames = new Array("Ahad","Isnin","Selasa","Rabu","Khamis","Jumaat","Sabtu");
+	var iMonthNames = new Array("Muharram","Safar","Rabiul Awal","Rabiul Akhir",
+	"Jamadil Awal","Jamadil Akhir","Rejab","Syaban",
+	"Ramadan","Syawal","Zulkaedah","Zhulhijjah");
+
+	
+	var iDate = kuwaiticalendar(adjustment);
+//	var outputIslamicDate = wdNames[iDate[4]] + ", " 
+//	+ iDate[5] + " " + iMonthNames[iDate[6]] + " " + iDate[7] + " AH";
+	var outputIslamicDate = idate[0] + " " + iDate[5] + " " + iMonthNames[iDate[6]] + " " + iDate[7];
+	
+	return outputIslamicDate;
 }
 
 function timeTo12HrFormat(time,ampm){   // Take a time in 24 hour format and format it in 12 hour format

f1f57b13f279915ac05be69f0567b6712f264f6a
lamlieabdullah <76871118+lamlieabdullah@users.noreply.github.com>
2021-04-06 00:32:28 +0800

    Update waktuSolat.php

--- waktuSolat.php
+++ waktuSolat.php
@@ -2,25 +2,30 @@
 header('Content-Type: application/json');
 
 if(isset($_GET['lokasi']))$lokasi = $_GET['lokasi'];
-else $lokasi = 'PLS01';
+else $lokasi = 'PHG03';
 
 if(isset($_GET['tahun']))$tahun = $_GET['tahun'];
-else $tahun = '2018';
+else $tahun = '2021';
 
 if(isset($_GET['bulan']))$bulan = $_GET['bulan'] + 1;
-else $bulan = '1';
+else $bulan = '5';
 
 if(isset($_GET['hari']))$hari = $_GET['hari'] - 1;
-else $hari = '0';
+else $hari = '17';
 
 // read incoming info and grab the chatID
 $content = file_get_contents("waktuSolat/".$tahun."/lokasi/" . $lokasi . ".json");
 $kandungan = json_decode($content, true);
 
-$keluaran = array();
+//$keluaran = array();
 
-$waktu_solat = $kandungan["$bulan"]["data"][$hari];
+$datee = mktime(0,0,0,$bulan,$hari,$tahun);
+//echo $datee->getTimestamp();
+echo date($datee);
 
+$key = array_search($datee, array_column($kandungan["prayer_times"],"datestamp"));
+
+$waktu_solat = $kandungan["prayer_times"][$key];
 
 $date =  $waktu_solat["date"];
 $imsak =  $waktu_solat["imsak"];

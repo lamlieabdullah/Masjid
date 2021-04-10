196eeb0a28de6688326577554f928a1b96e31d7d
lamlieabdullah <76871118+lamlieabdullah@users.noreply.github.com>
2021-04-10 20:03:51 +0800

    Update waktuSolat.php
    compute location based on lokasiID and time based on system time

--- waktuSolat.php
+++ waktuSolat.php
@@ -1,28 +1,17 @@
 <?php
-header('Content-Type: application/json');
+//header('Content-Type: application/json');
 
-if(isset($_GET['lokasi']))$lokasi = $_GET['lokasi'];
-else $lokasi = 'PHG03';
+$sql = "SELECT `lokasiID` FROM " . database_prefix ."_umum";
+$result = mysqli_query($dbc, $sql);
+if (mysqli_num_rows($result) === 1) {
+  $row = mysqli_fetch_assoc($result);
+  $lokasi = $row['lokasiID'];
+}
 
-if(isset($_GET['tahun']))$tahun = $_GET['tahun'];
-else $tahun = '2021';
-
-if(isset($_GET['bulan']))$bulan = $_GET['bulan'] + 1;
-else $bulan = '5';
-
-if(isset($_GET['hari']))$hari = $_GET['hari'] - 1;
-else $hari = '17';
-
-// read incoming info and grab the chatID
 $content = file_get_contents("waktuSolat/".$tahun."/lokasi/" . $lokasi . ".json");
 $kandungan = json_decode($content, true);
 
-//$keluaran = array();
-
 $datee = mktime(0,0,0);
-//$datee = mktime(0,0,0,date("m-d-yyyy"));
-//echo $datee->getTimestamp();
-// echo date($datee);
 
 $key = array_search($datee, array_column($kandungan["prayer_times"],"datestamp"));
 

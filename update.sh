af561508edf519f0fdaa749213884a5fce2d6cf7
lamlieabdullah <lamlie@hotmail.com>
2021-11-17 20:11:49 +0800

    update

---
+++ update.sh
@@ -0,0 +1,15 @@
+
+#!/bin/bash
+################################################################################
+#-------------------------------------------------------------------------------
+# Make a new file:
+# sudo nano install.sh
+# Place this content in it and then make the file executable:
+# sudo chmod +x install.sh
+# Execute the script to install:
+# ./install
+################################################################################
+
+cd /var/www/html/Masjid
+sudo wget https://raw.githubusercontent.com/lamlieabdullah/Masjid/script.php
+sudo wget https://raw.githubusercontent.com/lamlieabdullah/Masjid/kemaskini1-2.php

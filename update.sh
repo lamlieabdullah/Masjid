98dce555e835b70907d9388954255c1ce7dd7a92
lamlieabdullah <76871118+lamlieabdullah@users.noreply.github.com>
2021-12-04 10:47:23 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 ################################################################################
 #-------------------------------------------------------------------------------
 # Download file
-# sudo wget https://raw.githubusercontent.com/lamlieabdullah/Masjid/update.sh 
+# sudo wget https://raw.githubusercontent.com/lamlieabdullah/Masjid/main/update.sh 
 # Place this content in it and then make the file executable:
 # sudo chmod +x update.sh
 # Execute the script to install:
